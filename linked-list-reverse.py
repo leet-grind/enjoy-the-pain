253e73a1c2e8258cae96cc9189a7a5694fe84919
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2024-10-19 17:51:09 -0600

    Reverse Linked List

---
+++ linked-list-reverse.py
@@ -0,0 +1,14 @@
+def reverse_ll(head):
+  if not head:
+    return []
+  if not head.next:
+    return head
+  prev = None
+  curr = head
+  nxt = None
+  while curr:
+    nxt = curr.next
+    curr.next = prev
+    prev = curr
+    curr = nxt
+  return prev
